a42dd97b9602d0c4bece90afd983950fc5db57bb
dumpitca28 <dumpitca@outlook.com>
2021-08-17 05:55:27 +0800

    fixed the white navbar and its responsiveness

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,31 +4,43 @@
 @section('content')
 <link rel="stylesheet" href="/css/home.css">
 <!--White navbar-->
-
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-expand-lg bg-light p-3 lightnav">
     <div class="container-fluid">
-        <a class="navbar-brand"><img src="/images/navbarlogo.svg" width="150px" alt="white logo"></a>
-        <form class="row">
-            <div class="col-lg-5">
-                <input class="form-control me-2" type="search" placeholder="Search for an item">
-            </div>
-            <div class="col-lg-3">
-                <input class="form-control me-2" type="search" placeholder="All location">
-            </div>
-            <div class="col-lg-3">
-                <button class="btn btn-rent rounded-pill border-0 fs-5 fw-bold">Rent a bike</button>
-            </div>
+        <div class="col-lg-3 col-sm-12 mt-1">
+            <img src="images/navbarlogo.svg" id="whitelogo">
+        </div>
 
+        <form class="input-group">
+            <div class="col-lg-6 col-12 my-auto mt-2 ms-2">
+                <input type="search" class="form-control fw-bold" placeholder="Search for an item" id="search-bar">
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <div class="mt-1 input-group mb-3">
+                        <input type="text" class="location-bar form-control fw-bold" placeholder="All locations">
+                        <span class="input-group-text" id="basic-addon1">
+                            <i class="input-group-text bi bi-search"></i>
+                        </span>
+                    </div>
+                </div>
+               
+                <div class="col">
+                    <button class=" mx-auto btn btn-rent rounded-pill border-0 fs-5 fw-bold mt-1">
+                        Rent a bike
+                    </button>
+                </div>
+
+            </div>
         </form>
+
     </div>
 </nav>
-
 <!-- Banner  -->
 
 <div id="banner">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="open-cycles col-lg-6 p-5">
+            <div class="open-cycles col-lg-6 p-5 ms-4">
                 <h1 class="d-flex align-items-end display-1 fw-bolder">OPEN CYCLES</h1>
                 <button class="btn btn-now fst-italic fw-bolder rounded-pill">NOW AVAILABLE</button>
             </div>
@@ -36,7 +48,7 @@
     </div>
 </div>
 
-<div class="container">
+<div class="container-fluid ms-4">
     <div class="row">
         <div class="recommend col-lg-6 pt-3">
             <h1 class="fst-italic fw-bolder">Recommended for you:</h1>
@@ -46,15 +58,15 @@
 
 <!-- First Row -->
 
-<div class="container card-container shadow bg-body fw-bolder">
+<div class="container-fluid card-container shadow bg-body fw-bolder">
     <div class="row my-5">
         <div class="col-lg-3 col-sm-12 p-3">
             <div class="card card-course">
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -75,8 +87,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -96,8 +108,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -117,8 +129,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -138,8 +150,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -159,8 +171,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -180,8 +192,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -201,8 +213,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -222,8 +234,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -243,8 +255,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -264,8 +276,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
@@ -285,8 +297,8 @@
                 <div class="card-body">
                     <label>
                         <img src="/images/girl.png" alt="user photo" id="user-photo">
-                        Lucifer Morningstar                      
-                        <p class="mt-2 ms-4 lh-1">666 mins ago</p>           
+                        Lucifer Morningstar
+                        <p class="card-time mt-2 lh-3" id="card-time">666 mins ago</p>
                     </label>
 
                     <img src="/images/bike1.png" class="card-img-top" alt="bike">
