c263e9055d81ffe57aed46fe5fec653ea5fc30e1
dumpitca28 <dumpitca@outlook.com>
2021-10-18 02:38:25 +0800

    fixed white navbar

--- resources/views/template/search.blade.php
+++ resources/views/template/search.blade.php
@@ -1,26 +1,5 @@
-<style>
-@media (max-width: 575.98px) { 
-    #btn-rent {
-       margin-top: 2rem!important;
-    }
-    #white-logo {
-       display: none;
-    }
- } 
- @media (max-width: 991.98px) { 
-    #white-logo {
-       display: none;
-    }
-    #btn-rent {
-       margin-top: 2rem!important;
-    }
-  }
-
-  #btn-rent a {
-      color: #ffffff;
-
-  }
-</style>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/search.css">
 
 <nav class="navbar navbar-expand-lg bg-light lightnav">
     <div class="container-fluid">
@@ -31,20 +10,23 @@
             <div class="col-lg-6 col-md-7 col-12">
                 <input type="search" class="form-control fw-bold" placeholder="Search for an item" id="search-bar">
             </div>
-                <div class="col-lg-3 col-md-7" id="location">
-                    <div class="input-group">
-                        <input type="text" class="location-bar form-control fw-bold" placeholder="All locations">
-                        <span class="input-group-text" id="basic-addon1">
-                            <i class="input-group-text bi bi-search"></i>
-                        </span>
-                    </div>
-                </div>             
-                <div class="col-lg-3">
-                    <a class="text-light mx-auto btn btn-rent rounded-pill border-0 fs-5 fw-bold" id="btn-rent" 
+            <div class="col-lg-3 col-md-7" id="location">
+                <div class="input-group">
+                    <input type="text" class="location-bar form-control fw-bold" placeholder="All locations">
+                    <span class="input-group-text btn" id="basic-addon1">
+                        <i class="input-group-text bi bi-search"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <a class="text-light mx-auto btn rounded-pill border-0 fs-5 fw-bold" id="btn-rent"
                     href="{{url('product')}}">
                     Rent a bike
-                    </a>               
-                </div>     
+                </a>
+            </div>
+            <div class="col-lg-1">
+                <i class="fas fa-shopping-cart icon-color fs-5 pt-2" id="cart-size"></i>
+            </div>
         </form>
     </div>
 </nav>
