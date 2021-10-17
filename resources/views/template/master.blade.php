693e46d6aa36e73f5930fdbbe541ce49e1401c6c
dumpitca28 <dumpitca@outlook.com>
2021-10-18 02:37:12 +0800

    fixed favicon

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -13,7 +13,7 @@
     href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
     rel="stylesheet">
     <title>Bikers</title>
-    <link rel="icon" href="Images/Bikers_Logo.png">
+    <link rel="icon" href="Images/Bikers_favicon.png">
     
 
     <!-- Bootrsrap CSS -->
