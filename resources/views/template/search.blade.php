0564d9b3fa1e49dd037593cfa65e39e9df2a14b6
dumpit-ca <dumpitca@outlook.com>
2021-10-22 01:58:27 +0800

    cass -added modal to marketplace and product preview page. added href to logos

--- resources/views/template/search.blade.php
+++ resources/views/template/search.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid pe-0 pe-lg-3 ps-4 ps-lg-3">
             <div class="row align-items-center w-100">
                 <div class="col-lg-3 text-lg-start text-center">
-                    <img src="/images/navbarlogo.svg" alt="" class="img-fluid">
+                    <a href="{{url('')}}"><img src="/images/navbarlogo.svg" alt="" class="img-fluid"></a>       
                 </div>
                 <div id="search-item" class="col-lg-4 mb-3 mb-lg-0">
                     <input type="search" class="form-control fw-bold" placeholder="Search for an item" id="search-bar">
