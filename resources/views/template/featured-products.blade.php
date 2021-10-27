9c087d12f9de50e471b0eaff09c6f112ff22f375
dumpit-ca <dumpitca@outlook.com>
2021-10-27 19:06:43 +0800

    cass -added featured-products.blade.php to template

---
+++ resources/views/template/featured-products.blade.php
@@ -0,0 +1,66 @@
+<h1 class="text-lg-start text-center fw-bold">Feature Products</h1>
+<div class="card mb-3 border-0 mt-4" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="/images/bike1.png" class="img-fluid" alt="bike1">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Product Name</h5>
+                <p class="card-text">Price</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card mb-3 border-0 mt-4" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="/images/bike1.png" class="img-fluid" alt="bike1">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Product Name</h5>
+                <p class="card-text">Price</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card mb-3 border-0 mt-4" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="/images/bike1.png" class="img-fluid" alt="bike1">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Product Name</h5>
+                <p class="card-text">Price</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card mb-3 border-0 mt-4" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="/images/bike1.png" class="img-fluid" alt="bike1">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Product Name</h5>
+                <p class="card-text">Price</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card mb-3 border-0 mt-4" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="/images/bike1.png" class="img-fluid" alt="bike1">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Product Name</h5>
+                <p class="card-text">Price</p>
+            </div>
+        </div>
+    </div>
+</div>
